d47916bac768bc740673fcb026f36174ca9edab4
DESKTOP-L1APJUQ\thibaud <thibaud.staelens@student.unamur.be>
2017-09-25 15:28:14 -0700

    juntitestforeignkeys

--- DBTransformation/test/TestSQLQuery/JunitTest.java
+++ DBTransformation/test/TestSQLQuery/JunitTest.java
@@ -38,9 +38,17 @@ public class JunitTest {
         SQLQueryFactory sqlF = new SQLQueryFactory("localhost/mydb", "3306", "root", "root");
         int result = 0;
         try {
-            SQLCreateTableQuery add = sqlF.creatSQLCreateTableQuery("test", new String[]{"id int","name varchar(45)","trueFalse bool"});
-            add.sqlQueryDo();
-            add.sqlQueryUndo();
+            SQLCreateTableQuery add1 = sqlF.creatSQLCreateTableQuery("test", new String[]{"id int","name varchar(45)","trueFalse bool"});
+            add1.sqlQueryDo();
+            add1.sqlQueryUndo();
+            
+            ArrayList<Column> listCol = new ArrayList<>();
+            listCol.add(new Column("id","int")); listCol.add(new Column("name", "varchar(45)"));  listCol.add(new Column("trueFalse",  "bool"));
+            Table test2 = new Table("test2", listCol);
+            SQLCreateTableQuery add2 = sqlF.creatSQLCreateTableQuery(test2);
+            add2.sqlQueryDo();
+            add2.sqlQueryUndo();      
+            
             System.out.println("ok");
         } catch (SQLException ex) {
             System.out.println(ex);
@@ -56,10 +64,21 @@ public class JunitTest {
         int result = 0;
         try {
             sqlF.creatSQLCreateTableQuery("test", new String[]{"id int","name varchar(45)","trueFalse bool"}).sqlQueryDo();
-            SQLDropTableQuery drop = sqlF.creatDropTableQuery("test");
-            drop.sqlQueryDo();
-            drop.sqlQueryUndo();
-            drop.sqlQueryDo();
+            SQLDropTableQuery drop1 = sqlF.creatDropTableQuery("test");
+            drop1.sqlQueryDo();
+            drop1.sqlQueryUndo();
+            drop1.sqlQueryDo();
+            
+            ArrayList<Column> listCol = new ArrayList<>();
+            listCol.add(new Column("id","int")); listCol.add(new Column("name", "varchar(45)"));  listCol.add(new Column("trueFalse",  "bool"));
+            Table test2 = new Table("test2", listCol);
+            sqlF.creatSQLCreateTableQuery(test2).sqlQueryDo();
+            
+            SQLDropTableQuery drop2 = sqlF.creatDropTableQuery("test2");
+            drop2.sqlQueryDo();
+            drop2.sqlQueryUndo();
+            drop2.sqlQueryDo();
+            
             System.out.println("ok");
         } catch (SQLException ex) {
             System.out.println(ex);
@@ -122,19 +141,26 @@ public class JunitTest {
         }
         assertEquals(0, result);
     }
-    /*
+    
     @Test
     public void AlterAddForeignKeyQuery(){
         SQLQueryFactory sqlF = new SQLQueryFactory("localhost/mydb", "3306", "root", "root");
         int result = 0;
         try{
-            sqlF.creatSQLCreateTableQuery("test1", new String[]{"id int","name varchar(45)","trueFalse bool"}).sqlQueryDo();
-            sqlF.creatSQLCreateTableQuery("test2", new String[]{"id int","name varchar(45)","id1 int"}).sqlQueryDo();
             
-            ArrayList<Column> listCol = new ArrayList<>();
-            listCol.add(new Column("id", "int")); listCol.add(new Column("trueFalse", "bool"));
-            Table tb = new Table("test1", listCol);
-            sqlF.creatSQLAlterAddForeignKeyQuery("test2", "FK1", new Column("idl", "int"), tb);
+            
+            ArrayList<Column> listCol1 = new ArrayList(); listCol1.add(new Column("id","int")); listCol1.add(new Column("name", "varchar(45)"));  listCol1.add(new Column("trueFalse",  "bool"));
+            Table t1 = new Table("test13", listCol1);
+            SQLCreateTableQuery add1 = sqlF.creatSQLCreateTableQuery(t1);
+            add1.sqlQueryDo();
+            
+            ArrayList<Column> listCol2 = new ArrayList(); listCol2.add(new Column("id","int")); listCol2.add(new Column("name", "varchar(45)"));  listCol2.add(new Column("reference",  "int"));
+            Table t2 = new Table("test23", listCol2);
+            SQLCreateTableQuery add2 = sqlF.creatSQLCreateTableQuery(t2);
+            add2.sqlQueryDo();
+            
+            sqlF.creatSQLAlterAddPrimaryKeyQuery("test13", "id").sqlQueryDo();
+            sqlF.creatSQLAlterAddForeignKeyQuery("test23", "FK1", new Column("reference", "int"), "test13", "id").sqlQueryDo();
             
             //sqlF.creatDropTableQuery("test1").sqlQueryDo();
             //sqlF.creatDropTableQuery("test2").sqlQueryDo();
@@ -145,7 +171,7 @@ public class JunitTest {
             result = 1;
         }
         assertEquals(0, result);
-    }*/
+    }
     
     
     
