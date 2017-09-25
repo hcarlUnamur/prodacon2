99df10922aea2fdf14108ded7e4330095920ef49
DESKTOP-L1APJUQ\thibaud <thibaud.staelens@student.unamur.be>
2017-09-25 11:15:47 -0700

    junit

--- DBTransformation/test/TestSQLQuery/JunitTest.java
+++ DBTransformation/test/TestSQLQuery/JunitTest.java
@@ -5,6 +5,7 @@
  */
 package TestSQLQuery;
 
+import SQLQuery.StringTool;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,36 +18,25 @@ import static org.junit.Assert.*;
  * @author thibaud
  */
 public class JunitTest {
-    
-    public JunitTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
     // @Test
     // public void hello() {}
     
+    public String Iolo (String[] liss){
+        String hihi = "";
+        for(int i = 0; i < liss.length; i++){
+            hihi = hihi + liss[i];
+        }
+        return hihi;
+    }
+    
     @Test
     public void testHelloWorld() {
         System.out.println("* UtilsJUnit3Test: test method 1 - testHelloWorld()");
-        assertEquals("Hello, world!", "Hello" + ", " + "world" + "!");
+        assertEquals("Hello, world!", Iolo(new String[]{"Hello,", " ", "world!"}));
     }
     
+   
 }
