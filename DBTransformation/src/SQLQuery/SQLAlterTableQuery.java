0c1b35cfb224b4fd977a2271ca5fbc502aa8f6c8
hcarl <henry.carl@student.unamur.be>
2017-09-26 15:30:54 -0700

    add undo alter table

--- DBTransformation/src/SQLQuery/SQLAlterTableQuery.java
+++ DBTransformation/src/SQLQuery/SQLAlterTableQuery.java
@@ -1,9 +1,12 @@
 package SQLQuery;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class SQLAlterTableQuery extends SQLStructuresQuery{
@@ -14,6 +17,7 @@ public class SQLAlterTableQuery extends SQLStructuresQuery{
     private Column column;
     private Table referentialTable;
     private String constraintName;
+    private ResultSet datasave;
 
     public SQLAlterTableQuery(String[] table, Connection con) {
         super(table, con);
@@ -103,16 +107,29 @@ public class SQLAlterTableQuery extends SQLStructuresQuery{
         return out;
     }
     
-    private String createQuery(){
+    
+    private String createQueryAndSaveData(){
         String out="";
         switch(alteration){
             case AddColumn: 
                 out=String.format(sqlQuery, getTable()[0], "ADD", column.toString());
                 break;
-            case DropColumn: 
+            case DropColumn:
+                SQLSelectQuery select = new SQLSelectQuery(new String[]{"information_schema.columns"}, getCon(), new String[]{"column_name","column_type"},"table_name='"+getTable()[0]+"' AND column_name='"+column.getColumnName()+"'");       
+                try {
+                    datasave = select.sqlQueryDo();
+                } catch (SQLException ex) {
+                    Logger.getLogger(SQLAlterTableQuery.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 out=String.format(sqlQuery, getTable()[0], "DROP COLUMN", column.getColumnName());
                 break;
-            case ModifyColumnType: 
+            case ModifyColumnType:
+                SQLSelectQuery select2 = new SQLSelectQuery(new String[]{"information_schema.columns"}, getCon(), new String[]{"column_name","column_type"},"table_name='"+getTable()[0]+"' AND column_name='"+column.getColumnName()+"'");       
+                try {
+                    datasave = select2.sqlQueryDo();
+                } catch (SQLException ex) {
+                    Logger.getLogger(SQLAlterTableQuery.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 out=String.format(sqlQuery, getTable()[0], "MODIFY COLUMN", column.toString());
                 break;
             case AddForeignKey: 
@@ -123,13 +140,55 @@ public class SQLAlterTableQuery extends SQLStructuresQuery{
                         referentialTable.getName()+'('+((referentialTable.getTablecolumn()).get(0)).getColumnName()+')'
                 );
                 break;
-            case DropForeignKey: 
+            case DropForeignKey:
+                /*
+                SELECT 
+                    TABLE_NAME,COLUMN_NAME,CONSTRAINT_NAME, REFERENCED_TABLE_NAME,REFERENCED_COLUMN_NAME
+                FROM
+                    INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+                WHERE
+                    REFERENCED_TABLE_NAME = 'x' AND CONSTRAINT_NAME='y'   ;
+                */
+                SQLSelectQuery select3 = new SQLSelectQuery(
+                        new String[]{"INFORMATION_SCHEMA.KEY_COLUMN_USAGE"},
+                        getCon(),
+                        new String[]{"TABLE_NAME,COLUMN_NAME","COLUMN_NAME","CONSTRAINT_NAME","REFERENCED_TABLE_NAME","REFERENCED_COLUMN_NAME"},
+                        "REFERENCED_TABLE_NAME = '"+getTable()[0]+"' AND CONSTRAINT_NAME='"+constraintName+"'"
+                ); 
+        
+                try {
+                    datasave = select3.sqlQueryDo();
+                } catch (SQLException ex) {
+                    Logger.getLogger(SQLAlterTableQuery.class.getName()).log(Level.SEVERE, null, ex);
+                }
+   
                 out=String.format(sqlQuery, getTable()[0], "DROP FOREIGN KEY", constraintName,"");
                 break;
             case AddPrimaryKey:
                 out=String.format(sqlQuery, getTable()[0], "ADD PRIMARY KEY", "("+column.getColumnName()+")");
                 break;
             case DropPrimaryKey:
+                /*
+                SELECT
+                    COLUMN_NAME
+                FROM 
+                    INFORMATION_SCHEMA.COLUMNS
+                WHERE
+                    TABLE_NAME = 'cours'
+                    AND COLUMN_KEY = 'PRI';
+                */
+                SQLSelectQuery select4 = new SQLSelectQuery(
+                        new String[]{"INFORMATION_SCHEMA.COLUMNS"},
+                        getCon(),
+                        new String[]{"COLUMN_NAME"},
+                        "TABLE_NAME = '"+getTable()[0]+"' AND COLUMN_KEY = 'PRI'"
+                ); 
+        
+                try {
+                    datasave = select4.sqlQueryDo();
+                } catch (SQLException ex) {
+                    Logger.getLogger(SQLAlterTableQuery.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 out=String.format(sqlQuery, getTable()[0], "DROP PRIMARY KEY", "");
                 break;                
         }
@@ -141,7 +200,7 @@ public class SQLAlterTableQuery extends SQLStructuresQuery{
     @Override
     public Object sqlQueryDo() throws SQLException {
         Statement stmt = this.getCon().createStatement();
-        String query = createQuery();
+        String query = createQueryAndSaveData();
         stmt.executeUpdate(query);
         try{
          if(stmt!=null)
@@ -155,25 +214,36 @@ public class SQLAlterTableQuery extends SQLStructuresQuery{
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         switch(alteration){
             case AddColumn: 
-                
+                SQLAlterTableQuery dropC = CreateDropColumnQuery(getTable()[0], getCon(), column);
+                dropC.sqlQueryDo();
                 break;
             case DropColumn: 
-                
+                datasave.next();
+                SQLAlterTableQuery addC = CreateAddColumnQuery(getTable()[0], getCon(),new Column(datasave.getString(1), datasave.getString(2)));
+                addC.sqlQueryDo();
                 break;
             case ModifyColumnType: 
-                
+                datasave.next();
+                SQLAlterTableQuery mod = CreateModifyColumnTypeQuery(getTable()[0], getCon(),new Column(datasave.getString(1), datasave.getString(2)));
+                mod.sqlQueryDo();
                 break;
             case AddForeignKey: 
-                
+                SQLAlterTableQuery dropfk = CreateDroporeignKeyQuery(getTable()[0], getCon(), constraintName);
+                dropfk.sqlQueryDo();
                 break;
             case DropForeignKey: 
-    
+                //TABLE_NAME,COLUMN_NAME,CONSTRAINT_NAME, REFERENCED_TABLE_NAME,REFERENCED_COLUMN_NAME
+                datasave.next();
+                SQLAlterTableQuery addfk = CreateAddForeignKeyQuery(getTable()[0], getCon(), constraintName, new Column(datasave.getString("COLUMN_NAME"),null), datasave.getString("REFERENCED_TABLE_NAME"), datasave.getString("REFERENCED_COLUMN_NAME"));
+                addfk.sqlQueryDo();
                 break;
             case AddPrimaryKey:
-                
+                SQLAlterTableQuery dropPK = CreateDropPrimaryKeyQuery(getTable()[0], getCon());
+                dropPK.sqlQueryDo();
                 break;
             case DropPrimaryKey:
-                
+                SQLAlterTableQuery addpk = CreateAddPrimaryKeyQuery(getTable()[0], getCon(), datasave.getString("COLUMN_NAME"));
+                addpk.sqlQueryDo();
                 break;                
         }
         return null;
