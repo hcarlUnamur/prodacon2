434c3ff9230bd8e21cbea8ac8c2a1bb1771087f6
hcarl <henry.carl@student.unamur.be>
2017-09-26 09:45:41 -0700

    add undo delet

--- DBTransformation/src/SQLQuery/SQLDeleteQuery.java
+++ DBTransformation/src/SQLQuery/SQLDeleteQuery.java
@@ -6,8 +6,12 @@
 package SQLQuery;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,15 +20,22 @@ import java.sql.Statement;
 public class SQLDeleteQuery extends SQLManipulationQuery{
 
     private static String QUERYFORMAT = "DELETE FROM %s WHERE (%s)"; 
-    private String[][] whereValues;
+    private String whereValues;
+    private ResultSet datasave;
     
     public SQLDeleteQuery(String[] table, Connection con) {
         super(table, con);
     }
     
-    public SQLDeleteQuery(String table, Connection con, String[][] whereValues){
+    public SQLDeleteQuery(String table, Connection con, String whereValues){
         super(new String[]{table}, con);
         this.whereValues = whereValues;
+        SQLSelectQuery select = new SQLSelectQuery(table, getCon(), new String[]{"*"}, whereValues);
+        try {
+            datasave = select.sqlQueryDo();
+        } catch (SQLException ex) {
+            Logger.getLogger(SQLDeleteQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     
@@ -34,7 +45,7 @@ public class SQLDeleteQuery extends SQLManipulationQuery{
         Statement stmt = this.getCon().createStatement();
         String s = getTable()[0];
         
-        String cond = StringTool.WhereToStringVal(whereValues);
+        String cond = whereValues;
         String query = String.format(QUERYFORMAT,s,cond);
         System.out.println(query);
         stmt.executeUpdate(query);
@@ -48,6 +59,18 @@ public class SQLDeleteQuery extends SQLManipulationQuery{
 
     @Override
     public Object sqlQueryUndo() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.         
+        ResultSetMetaData meta = datasave.getMetaData();
+        int length = meta.getColumnCount();
+        String[] cols = new String[length];
+        String[] values = new String[length];
+        for (int i=0;i<length;i++){
+            cols[i]= meta.getColumnClassName(i+1);
+            values[i] = datasave.getNString(i);
+        }
+        
+        SQLInsertQuery insert = new SQLInsertQuery(getTable()[0], getCon(), cols, values);
+        insert.sqlQueryDo();
+        return null;
     }
 }
