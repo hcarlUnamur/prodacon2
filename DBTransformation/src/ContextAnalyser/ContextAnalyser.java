30222076a9522568710430ed4bf7d6ca9df0a760
hcarl <henry.carl@student.unamur.be>
2017-10-04 15:57:21 -0700

    test

--- DBTransformation/src/ContextAnalyser/ContextAnalyser.java
+++ DBTransformation/src/ContextAnalyser/ContextAnalyser.java
@@ -86,7 +86,7 @@ public class ContextAnalyser {
         if (isTheSameType(fkColumn, referencedColumn)){
             System.out.println("*****************Same type but different length ");
             // test if same type ( a one parameter type) but with different length
-            System.out.println(getTypelength1(referencedColumn) +" / "+ getTypelength1(fkColumn));
+            System.out.println(referencedColumn.getColumnName().split("(|)|,")[1] +" / "+ fkColumn.getColumnType().split("(|)|,")[1]);
             if (isIn(getTypeName(fkColumn), ONE_PARAMETER_TYPE)){
                 System.out.println("*****************Case 1 ");
                 if (getTypelength1(referencedColumn)>getTypelength1(fkColumn)){
