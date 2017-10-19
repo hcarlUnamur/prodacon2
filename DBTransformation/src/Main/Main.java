92cf8ece8d87648eb35d89f1ba3389c17b880fa2
hcarl <henry.carl@student.unamur.be>
2017-10-19 12:27:56 -0700

    test desable info log

--- DBTransformation/src/Main/Main.java
+++ DBTransformation/src/Main/Main.java
@@ -53,6 +53,7 @@ public class Main {
     }
     
     public static void main(String[] args) {
+        Logger.getGlobal().setLevel(Level.SEVERE);
         args = "-dbhost localhost -dbname mydb -dbport 3306 -dblogin carl -dbpw root -fkfile ./possible_matches.txt".split(" ");
         Main main = new Main(args);
         main.mainMenu();
