6fe0265826f0e195cff37671673ae2eef350569b
hcarl <henry.carl@student.unamur.be>
2017-10-30 14:40:35 -0700

    update gui

--- Prodacon2GUI/src/prodacon2gui/MainController.java
+++ Prodacon2GUI/src/prodacon2gui/MainController.java
@@ -138,8 +138,8 @@ public class MainController implements Initializable {
         
         cascadeTable.setItems(cascadeTransformationObservableList);
         unmatchingValue.setItems(unmatchingValueObservableList);
-        cascadeTableColumn.setCellValueFactory(cellData->new SimpleStringProperty(cellData.getValue().getReferencedTableName()));
-        cascadeColumnColumn.setCellValueFactory(cellData->new SimpleStringProperty(cellData.getValue().getReferencedColumn()));
+        cascadeTableColumn.setCellValueFactory(cellData->new SimpleStringProperty(cellData.getValue().getForeingKeyTable()+"."+cellData.getValue().getForeingKeyColumn()));
+        cascadeColumnColumn.setCellValueFactory(cellData->new SimpleStringProperty(cellData.getValue().getReferencedTableName()+"."+cellData.getValue().getReferencedColumn()));
         unmatchingValueColumn.setCellValueFactory(s->new SimpleStringProperty(s.getValue().toString()));
     }    
     
